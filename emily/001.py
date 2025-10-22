9d49614cc2d58c2840d63efb2dd569e2414c31fc
emily vitoria <emily456vit@gmail.com>
2025-10-22 07:27:56 -0300

    Update 001.py

---
+++ emily/001.py
@@ -0,0 +1,11 @@
+ano_nasc = (input("Digite o ano do seu aniversario"))
+ano_atual = int(input("Digite o ano em que estamosa "))
+soma = ano_nasc - ano_atual
+print("voce tem:",soma)
+idade = 16
+if(soma < idade):
+    print ("voce ainda fara 16 anos.")
+else:
+    if (soma <= idade):
+        print ("voce ja fez 16 anos!")
+      
